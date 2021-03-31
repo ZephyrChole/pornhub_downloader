fb871170ad9da3c4c770cbe294b91ce44a2138be
ZephyrChole <1905616103@qq.com>
2021-03-31 21:49:07 +0800

    Patch:Video url is removed before finished. Problem:Video url is removed before finished. Solution:Create back_up file before overwriting the original.

--- com/hebut/zephyrchole/pornhub_downloader/url_manager.py
+++ com/hebut/zephyrchole/pornhub_downloader/url_manager.py
@@ -6,6 +6,7 @@
 # @time: 2021/3/26 15:00
 import logging
 import time
+import os
 
 
 class UrlManager:
@@ -13,6 +14,7 @@ class UrlManager:
     def __init__(self, url_file_path, pool_capacity, level, download_url_queue, produce_url_queue, download_queue,
                  text_urls):
         self.url_file_path = url_file_path
+        self.back_up_path = '{}.bak'.format(url_file_path)
         self.pool_capacity = pool_capacity
         self.download_queue = download_queue
         self.text_urls = text_urls
@@ -31,7 +33,7 @@ class UrlManager:
         self.logger.setLevel(level)
         self.logger.addHandler(fh)
 
-    def read_in_urls(self, url_file_path):
+    def read_in(self, url_file_path):
         with open(url_file_path) as file:
             content = file.readlines()
             for url in content:
@@ -39,14 +41,31 @@ class UrlManager:
                 if len(url) > 0:
                     self.text_urls.append(url)
                     self.produce_url_queue.put(url)
+
+    def read_in_urls(self, url_file_path):
+
+        if os.path.exists(self.back_up_path):
+            self.read_in(self.back_up_path)
+            if os.path.exists(url_file_path):
+                os.remove(url_file_path)
+            os.rename(self.back_up_path, url_file_path)
+        else:
+            self.read_in(url_file_path)
+            if os.path.exists(self.back_up_path):
+                os.remove(self.back_up_path)
         self.logger.info('从本地文件: {} 中读取了 {} 个链接'.format(self.url_file_path, len(self.text_urls)))
+
+    def copy_file(self, src, dst):
+        os.system('cp {} {}'.format(src, dst))
 
     def remove_text_url(self, url):
         self.text_urls.remove(url)
+        self.copy_file(self.url_file_path, self.back_up_path)
         with open(self.url_file_path, 'w') as file:
             for url in self.text_urls:
                 file.write(url)
                 file.write('\n')
+        os.remove(self.back_up_path)
 
     def notify(self):
         self.logger.info(
