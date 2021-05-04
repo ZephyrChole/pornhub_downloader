0fb09de79320bd926142df6a5a6941f18c8457f5
ZephyrChole <1905616103@qq.com>
2021-05-04 23:55:25 +0800

    Optimized: removed os.chdir

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 # Author:Jiawei Feng
 # @software: PyCharm
 # @file: pornhub_downloader.py
-# @time: 2021/3/26 15:50 
-from os import chdir
+# @time: 2021/3/26 15:50
 import com.hebut.zephyrchole.pornhub_downloader.pornhub_downloader as pd
 
 
 def main():
-    chdir('/media/pi/sda1/media/porn/pornhub_downloader')
     pd.main('../unsorted', './input.txt', 5, 'DEBUG', ['../repo'])
 
 
